816d016c0fe474681c6ff90eb451a76095b533ff
Leonardo Marques <leommarques7@gmail.com>
2015-03-07 04:18:01 -0300

    btn enviar msg

--- src/leod7k/quizmica/servidor/Main.java
+++ src/leod7k/quizmica/servidor/Main.java
@@ -1,14 +1,13 @@
 package leod7k.quizmica.servidor;
 
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
-
 import javax.swing.JButton;
 import javax.swing.JTextArea;
 
 import leod7k.quizmica.servidor.gui.ServerGUI;
 import leod7k.quizmica.servidor.gui.listeners.BtnAbrirListener;
+import leod7k.quizmica.servidor.gui.listeners.BtnComecarListener;
 import leod7k.quizmica.servidor.gui.listeners.BtnTerminarListener;
+import leod7k.quizmica.servidor.gui.listeners.WindowCloser;
 
 public class Main {
 
@@ -17,69 +16,19 @@ public class Main {
 		ServerGUI serverGUI = new ServerGUI();
 		JTextArea textArea = serverGUI.getTextArea();
 		JButton btnAbrir = serverGUI.getBtnAbrir();
-		//JButton btnComecar = serverGUI.getBtnComecar();
+		JButton btnComecar = serverGUI.getBtnComecar();
 		JButton btnTerminar = serverGUI.getBtnTerminar();
 
 		Connections con = new Connections();
 
 		btnAbrir.addActionListener(new BtnAbrirListener(con, textArea));
 		
+		btnComecar.addActionListener(new BtnComecarListener(con, textArea));
+		
 		BtnTerminarListener btl = new BtnTerminarListener(con, textArea);
 		btnTerminar.addActionListener(btl);
 
-		serverGUI.addWindowListener(new WindowCLoser(btl));
-	}
-
-}
-
-class WindowCLoser implements WindowListener {
-
-	private BtnTerminarListener btl ;
-
-	public WindowCLoser(BtnTerminarListener paramBtl) {
-		btl = paramBtl;
-	}
-
-	@Override
-	public void windowActivated(WindowEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void windowClosed(WindowEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void windowClosing(WindowEvent e) {
-		btl.actionPerformed(null);
-        System.exit(0);
-	}
-
-	@Override
-	public void windowDeactivated(WindowEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void windowDeiconified(WindowEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void windowIconified(WindowEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void windowOpened(WindowEvent e) {
-		// TODO Auto-generated method stub
-
+		serverGUI.addWindowListener(new WindowCloser(btl));
 	}
 
 }
