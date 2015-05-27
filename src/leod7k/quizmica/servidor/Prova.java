76312e11a1bca61d2661dbe6aa682bd23e20dad3
Leonardo Marques <leommarques7@gmail.com>
2015-05-27 13:18:09 -0300

    Escreve as respostas dos alunos em um arquivo

--- src/leod7k/quizmica/servidor/Prova.java
+++ src/leod7k/quizmica/servidor/Prova.java
@@ -1,6 +1,9 @@
 package leod7k.quizmica.servidor;
 
 import java.awt.event.WindowEvent;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.swing.JTextArea;
@@ -9,7 +12,7 @@ import leod7k.quizmica.servidor.gui.ProvaGUI;
 import leod7k.quizmica.servidor.gui.listeners.BtnPassarListener;
 
 public class Prova {
-	
+
 	private Connections con;
 	private int q;
 	private ProvaGUI gui;
@@ -19,12 +22,12 @@ public class Prova {
 	public Prova(Connections paramCon, JTextArea paramTextArea) {
 		con = paramCon;
 		q = -1;
-		
+
 		addQuestoes();
-		
+
 		textArea = paramTextArea;
 		gui = new ProvaGUI();
-		
+
 		gui.getBtnPassar().addActionListener(new BtnPassarListener(this));
 		proxima();
 	}
@@ -39,8 +42,7 @@ public class Prova {
 		qTexts.add("<html> <h3>2) De acordo com Rutherford, o átomo consiste nas duas seguintes partes</h3><p>"
 				+ "A) Elétrons e Prótons<p>"
 				+ "B) Núcleo e a parte extra-nuclear<p>"
-				+ "C) Nêutrons e Elétrons<p>"
-				+ "D) Prótons e Nêutrons </html>");
+				+ "C) Nêutrons e Elétrons<p>" + "D) Prótons e Nêutrons </html>");
 		qTexts.add("<html> <h3>3) As três partículas fundamentais do átomo são</h3><p>"
 				+ "A) núcleo, Anti-quarks e Elétrons<p>"
 				+ "B) Mésons, Quarks e Anti-neutrino<p>"
@@ -50,53 +52,83 @@ public class Prova {
 
 	public void proxima() {
 		if (++q < 3) {
-			textArea.setText("Questão " + (q+1) + "\n");
-			con.getServerDispatcher().serverMessage(q+"");
+			textArea.setText("Questão " + (q + 1) + "\n");
+			con.getServerDispatcher().serverMessage(q + "");
 			gui.getLabel().setText(qTexts.get(q));
 		} else {
 			gui.dispatchEvent(new WindowEvent(gui, WindowEvent.WINDOW_CLOSING));
 			con.getServerDispatcher().serverMessage("fim");
-			
+
 			textArea.setText("Fim da prova\n");
 			textArea.setText("Resultado:\n");
-			textArea.setText(con.getServerDispatcher().getClientCount() + " alunos fizeram a prova.\n");
+			textArea.setText(con.getServerDispatcher().getClientCount()
+					+ " alunos fizeram a prova.\n");
 
-			int[][] resultado =  new int[3][4];
-			for (ClientInfo clientInfo: con.getServerDispatcher().getmClients()) {
-				try {
+			int[][] resultado = new int[3][4];
+			BufferedWriter out = null;
+
+			try {
+				out = new BufferedWriter(new FileWriter("respostas.txt"));
+
+				for (ClientInfo clientInfo : con.getServerDispatcher()
+						.getmClients()) {
+					out.write(clientInfo.toString() + ":");
+					out.newLine();
 					for (int i = 0; i < 3; i++) {
-						if (clientInfo.mClientListener.getRespostas().get(i).equals("'A'")) {
+						out.write((i + 1) + ") "
+								+ clientInfo.mClientListener.getRespostas()
+										.get(i));
+						out.newLine();
+						if (clientInfo.mClientListener.getRespostas().get(i)
+								.equals("'A'")) {
 							resultado[i][0]++;
 						}
-						if (clientInfo.mClientListener.getRespostas().get(i).equals("'B'")) {
+						if (clientInfo.mClientListener.getRespostas().get(i)
+								.equals("'B'")) {
 							resultado[i][1]++;
 						}
-						if (clientInfo.mClientListener.getRespostas().get(i).equals("'C'")) {
+						if (clientInfo.mClientListener.getRespostas().get(i)
+								.equals("'C'")) {
 							resultado[i][2]++;
 						}
-						if (clientInfo.mClientListener.getRespostas().get(i).equals("'D'")) {
+						if (clientInfo.mClientListener.getRespostas().get(i)
+								.equals("'D'")) {
 							resultado[i][3]++;
 						}
 					}
-				} catch (Exception e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
 				}
+				
+				out.close();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-			
+
 			for (int i = 0; i < 3; i++) {
 				textArea.append("Questão " + i + ":\n");
 				if (resultado[i][0] != 0) {
-					textArea.append(resultado[i][0] + " alunos (" + resultado[i][0] * 100 / con.getServerDispatcher().getClientCount() + "%) responderam 'A'\n");						
+					textArea.append(resultado[i][0] + " alunos ("
+							+ resultado[i][0] * 100
+							/ con.getServerDispatcher().getClientCount()
+							+ "%) responderam 'A'\n");
 				}
 				if (resultado[i][1] != 0) {
-					textArea.append(resultado[i][1] + " alunos (" + resultado[i][1] * 100 / con.getServerDispatcher().getClientCount() + "%) responderam 'B'\n");						
+					textArea.append(resultado[i][1] + " alunos ("
+							+ resultado[i][1] * 100
+							/ con.getServerDispatcher().getClientCount()
+							+ "%) responderam 'B'\n");
 				}
 				if (resultado[i][2] != 0) {
-					textArea.append(resultado[i][2] + " alunos (" + resultado[i][2] * 100 / con.getServerDispatcher().getClientCount() + "%) responderam 'C'\n");						
+					textArea.append(resultado[i][2] + " alunos ("
+							+ resultado[i][2] * 100
+							/ con.getServerDispatcher().getClientCount()
+							+ "%) responderam 'C'\n");
 				}
 				if (resultado[i][3] != 0) {
-					textArea.append(resultado[i][3] + " alunos (" + resultado[i][3] * 100 / con.getServerDispatcher().getClientCount() + "%) responderam 'D'\n");						
+					textArea.append(resultado[i][3] + " alunos ("
+							+ resultado[i][3] * 100
+							/ con.getServerDispatcher().getClientCount()
+							+ "%) responderam 'D'\n");
 				}
 				textArea.append("\n");
 			}
