772fdafa38f9fcb12279713dd5788744cca7b95e
Leonardo Marques <leommarques7@gmail.com>
2015-05-27 13:50:31 -0300

    Lê prova de arquivo.

--- src/leod7k/quizmica/servidor/Prova.java
+++ src/leod7k/quizmica/servidor/Prova.java
@@ -1,9 +1,10 @@
 package leod7k.quizmica.servidor;
 
 import java.awt.event.WindowEvent;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.swing.JTextArea;
@@ -13,6 +14,8 @@ import leod7k.quizmica.servidor.gui.listeners.BtnPassarListener;
 
 public class Prova {
 
+	private static final int QNT_ALTERNATIVAS = 4;
+	private static final int QNT_QUESTOES = 3;
 	private Connections con;
 	private int q;
 	private ProvaGUI gui;
@@ -34,24 +37,41 @@ public class Prova {
 
 	private void addQuestoes() {
 		qTexts = new ArrayList<String>();
-		qTexts.add("<html> <h3>1) Como resultado da experiência de Rutherford, foi concluído que</h3><p>"
-				+ "A) o centro do átomo é vazio.<p>"
-				+ "B) a massa atômica é distribuída por todo o átomo.<p>"
-				+ "C) o centro do átomo tem carga negativa.<p>"
-				+ "D) a maior parte do átomo é vazia. </html>");
-		qTexts.add("<html> <h3>2) De acordo com Rutherford, o átomo consiste nas duas seguintes partes</h3><p>"
-				+ "A) Elétrons e Prótons<p>"
-				+ "B) Núcleo e a parte extra-nuclear<p>"
-				+ "C) Nêutrons e Elétrons<p>" + "D) Prótons e Nêutrons </html>");
-		qTexts.add("<html> <h3>3) As três partículas fundamentais do átomo são</h3><p>"
-				+ "A) núcleo, Anti-quarks e Elétrons<p>"
-				+ "B) Mésons, Quarks e Anti-neutrino<p>"
-				+ "C) Positron, Neutrino e Raios Gama<p>"
-				+ "D) Próton, Nêutron e Elétron </html>");
+		BufferedReader reader = null;
+
+		try {
+		    reader = new BufferedReader(new FileReader("prova.txt"));
+		    
+		    String temp;
+		    while ((temp = reader.readLine()) != null) {
+		    	String text = "<html> <h3>" + temp + "</h3><p>";
+		    	for (int i = 0; i < QNT_ALTERNATIVAS; i++)
+		    		text += reader.readLine() + "<p>";
+		    	qTexts.add(text + "</html>");
+		    }
+		    
+		    reader.close();
+		} catch (Exception e) {
+			qTexts.clear();
+			qTexts.add("<html> <h3>1) Como resultado da experiência de Rutherford, foi concluído que</h3><p>"
+					+ "A) o centro do átomo é vazio.<p>"
+					+ "B) a massa atômica é distribuída por todo o átomo.<p>"
+					+ "C) o centro do átomo tem carga negativa.<p>"
+					+ "D) a maior parte do átomo é vazia. </html>");
+			qTexts.add("<html> <h3>2) De acordo com Rutherford, o átomo consiste nas duas seguintes partes</h3><p>"
+					+ "A) Elétrons e Prótons<p>"
+					+ "B) Núcleo e a parte extra-nuclear<p>"
+					+ "C) Nêutrons e Elétrons<p>" + "D) Prótons e Nêutrons </html>");
+			qTexts.add("<html> <h3>3) As três partículas fundamentais do átomo são</h3><p>"
+					+ "A) núcleo, Anti-quarks e Elétrons<p>"
+					+ "B) Mésons, Quarks e Anti-neutrino<p>"
+					+ "C) Positron, Neutrino e Raios Gama<p>"
+					+ "D) Próton, Nêutron e Elétron </html>");
+		}
 	}
 
 	public void proxima() {
-		if (++q < 3) {
+		if (++q < QNT_QUESTOES) {
 			textArea.setText("Questão " + (q + 1) + "\n");
 			con.getServerDispatcher().serverMessage(q + "");
 			gui.getLabel().setText(qTexts.get(q));
@@ -64,7 +84,7 @@ public class Prova {
 			textArea.setText(con.getServerDispatcher().getClientCount()
 					+ " alunos fizeram a prova.\n");
 
-			int[][] resultado = new int[3][4];
+			int[][] resultado = new int[QNT_QUESTOES][QNT_ALTERNATIVAS];
 			BufferedWriter out = null;
 
 			try {
@@ -74,7 +94,7 @@ public class Prova {
 						.getmClients()) {
 					out.write(clientInfo.toString() + ":");
 					out.newLine();
-					for (int i = 0; i < 3; i++) {
+					for (int i = 0; i < QNT_QUESTOES; i++) {
 						out.write((i + 1) + ") "
 								+ clientInfo.mClientListener.getRespostas()
 										.get(i));
@@ -104,7 +124,7 @@ public class Prova {
 				e.printStackTrace();
 			}
 
-			for (int i = 0; i < 3; i++) {
+			for (int i = 0; i < QNT_QUESTOES; i++) {
 				textArea.append("Questão " + i + ":\n");
 				if (resultado[i][0] != 0) {
 					textArea.append(resultado[i][0] + " alunos ("
